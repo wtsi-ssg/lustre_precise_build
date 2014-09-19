df8d4718dbe68ecaaa7d8976cbbbb41f3b39c7d5
James Beal <james.beal@sanger.ac.uk>
2014-09-19 11:28:10 +0100

    really build all

--- lustre/scripts/build_all.sh
+++ lustre/scripts/build_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Output redirected to  /vagrant/lustre/LOG 
-#rm -f  /vagrant/lustre/LOG
-#bash  /vagrant/lustre/scripts/download_source.sh  >> /vagrant/lustre/LOG  2>&1 || exit
-#bash  /vagrant/lustre/scripts/unpack_source.sh    >> /vagrant/lustre/LOG  2>&1  || exit 
-#bash  /vagrant/lustre/scripts/prepare_source.sh   >> /vagrant/lustre/LOG  2>&1  || exit
-#bash  /vagrant/lustre/scripts/compile_linux.sh    >> /vagrant/lustre/LOG  2>&1 || exit 
+rm -f  /vagrant/lustre/LOG
+bash  /vagrant/lustre/scripts/download_source.sh  >> /vagrant/lustre/LOG  2>&1 || exit
+bash  /vagrant/lustre/scripts/unpack_source.sh    >> /vagrant/lustre/LOG  2>&1 || exit 
+bash  /vagrant/lustre/scripts/prepare_source.sh   >> /vagrant/lustre/LOG  2>&1 || exit
+bash  /vagrant/lustre/scripts/compile_linux.sh    >> /vagrant/lustre/LOG  2>&1 || exit 
 bash  /vagrant/lustre/scripts/compile_lustre.sh   >> /vagrant/lustre/LOG  2>&1 || exit

5eeefbde9975face04f3c40e966b19999bc1dfd8
James Beal <james.beal@sanger.ac.uk>
2014-10-22 14:46:02 +0100

    Chnge the debian rules file to be consistent with lucid build world

--- lustre/scripts/compile_lustre.sh
+++ lustre/scripts/compile_lustre.sh
@@ -3,6 +3,7 @@ cd /vagrant/lustre/source/lustre-2.5.19/debian
 echo Y | ./prepare.sh /vagrant/lustre/debs/linux-image-*
 cd /vagrant/lustre/source/lustre-2.5.19
 dpkg-buildpackage -rfakeroot -uc -us
+mv /vagrant/lustre/source/*.deb /vagrant/lustre/debs/
 rm -rf  /var/lib/dkms/lustre-exascaler-2-1-client-modules
 dh_clean
 make clean
--- lustre/scripts/prepare_source.sh
+++ lustre/scripts/prepare_source.sh
@@ -16,17 +16,4 @@ cat /vagrant/lustre/changes/patches/config.patch | patch -R  .config || exit 4
 cd /vagrant/lustre/source/lustre-2.5.19
 echo "Copying debian rules in to place"
 rsync -av /vagrant/lustre/changes/debian .
-mv debian/dmks.conf .
-echo "Apply patches to get lustre to build on ubuntu and gcc 4.6"
-echo "These patches turn off -Wformat-security in the minimal amount of code in" 
-echo "each file so they compile with gcc 4.6.x"
-cat /vagrant/lustre/changes/patches/ext4-mmp.patch.patch  | patch /vagrant/lustre/source/lustre-2.5.19/ldiskfs/kernel_patches/patches/rhel6.4/ext4-mmp.patch  || exit 5
-cat /vagrant/lustre/changes/patches/pinger.c.patch        | patch -R  /vagrant/lustre/source/lustre-2.5.19/lustre/ptlrpc/pinger.c || exit 6
-cat /vagrant/lustre/changes/patches/ldlm_lockd.c.patch    | patch ./lustre/ldlm/ldlm_lockd.c || exit 7
-cat /vagrant/lustre/changes/patches/o2iblnd_cb.c.patch    | patch ./lnet/klnds/o2iblnd/o2iblnd_cb.c || exit 8
-cat /vagrant/lustre/changes/patches/ptlrpcd.c.patch       | patch ./lustre/ptlrpc/ptlrpcd.c || exit 9
-cat /vagrant/lustre/changes/patches/qsd_reint.c.patch     | patch ./lustre/quota/qsd_reint.c || exit 10
-cat /vagrant/lustre/changes/patches/service.c.patch       | patch ./lustre/ptlrpc/service.c || exit 11
-cat /vagrant/lustre/changes/patches/socklnd_cb.c.patch    | patch ./lnet/klnds/socklnd/socklnd_cb.c || exit 12
-cat /vagrant/lustre/changes/patches/workitem.c.patch      | patch ./libcfs/libcfs/workitem.c || exit 13
-
+mv /vagrant/lustre/source/lustre-2.5.19/debian/dkms.conf /vagrant/lustre/source/lustre-2.5.19/

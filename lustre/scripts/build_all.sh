d1fe51948ffd0646a1c5fd1c27b06c8eb195316b
James Beal <james.beal@sanger.ac.uk>
2015-03-23 16:43:31 +0000

    This is what we built the server on

--- lustre/scripts/build_all.sh
+++ lustre/scripts/build_all.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo Output redirected to  /vagrant/lustre/LOG 
 rm -f  /vagrant/lustre/LOG
-export LUSTREVER=lustre-2.5.27.ddnpf2
-export LUSTRE_VERSION=2.5.27.ddnpf2
+export LUSTREVER=lustre-2.5.29.ddnpf3
+export LUSTRE_VERSION=2.5.29.ddnpf3
 bash  /vagrant/lustre/scripts/download_source.sh  >> /vagrant/lustre/LOG  2>&1 || exit
 bash  /vagrant/lustre/scripts/unpack_source.sh    >> /vagrant/lustre/LOG  2>&1 || exit 
 bash  /vagrant/lustre/scripts/prepare_source.sh   >> /vagrant/lustre/LOG  2>&1 || exit
 bash  /vagrant/lustre/scripts/compile_linux.sh    >> /vagrant/lustre/LOG  2>&1 || exit 
-exit
 bash  /vagrant/lustre/scripts/compile_lustre.sh   >> /vagrant/lustre/LOG  2>&1 || exit

416a07953386cdc8815780ddf77292a2402ed819
James Beal <james.beal@sanger.ac.uk>
2015-04-02 13:47:58 +0100

    Don't try and build the binary packages

--- lustre/scripts/compile_lustre.sh
+++ lustre/scripts/compile_lustre.sh
@@ -10,12 +10,5 @@ make clean
 echo building dkms packages
 rsync -a /vagrant/lustre/source/$LUSTREVER/*  /usr/src/lustre-client-modules-$LUSTRE_VERSION-1.0.0
 dkms add -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0 --all
-dkms build -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0  -k 3.8.0-44-generic  -k 3.2.0-75-generic
-dkms -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0 --all
-rm /var/lib/dkms/lustre-client-modules-${LUSTRE_VERSION}/1.0.0/tarball/lustre-client-modules-${LUSTRE_VERSION}-1.0.0.dkms.tar.gz
-yes | dkms mkdeb  -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0 --all
+dkms mkdeb  -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0 --source-only
 mv  /var/lib/dkms/lustre-client-modules-$LUSTRE_VERSION/1.0.0/deb/*.deb /vagrant/lustre/debs
-rm  /var/lib/dkms/$LUSTREVER-client-modules/1.0.0/tarball/*
-dkms mkdsc -m lustre-client-modules-$LUSTRE_VERSION  -v 1.0.0 --all
-mv  /var/lib/dkms//lustre-client-modules-$LUSTRE_VERSION/1.0.0/dsc/*.dsc /vagrant/lustre/debs
-

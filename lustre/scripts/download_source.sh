9427d76a04412b1fe220b5c68c1df0db1ce145a7
James Beal <James.Beal@sanger.ac.uk>
2015-09-18 09:20:08 +0100

    remove local fine

--- lustre/scripts/download_source.sh
+++ lustre/scripts/download_source.sh
@@ -21,10 +21,8 @@ if [  "`md5sum linux-2.6.32-431.17.1.el6.x86_64.tar.gz |  awk '{print $1}' `" !=
   rm -f linux-2.6.32-431.17.1.el6.x86_64.tar.gz
   wget ftp://ftp.sanger.ac.uk/pub/users/jb23/linux-2.6.32-431.17.1.el6.x86_64.tar.gz
 fi
- cp ~ubuntu/linux-2.6.32-573.3.1.el6.x86_64.tar.gz .
 if [  "`md5sum MLNX_OFED_LINUX-2.1-1.0.6-ubuntu12.04-x86_64.tgz |  awk '{print $1}'`" != "f590709dd4e9a31dd05251bcb66241cc"  ] ; then
   rm -f  MLNX_OFED_LINUX-2.1-1.0.6-ubuntu12.04-x86_64.tgz
   #wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-2.1-1.0.6/MLNX_OFED_LINUX-2.1-1.0.6-ubuntu12.04-x86_64.tgz
   wget ftp://ftp.sanger.ac.uk/pub/jb23/MLNX_OFED_LINUX-2.1-1.0.6-ubuntu12.04-x86_64.tgz
 fi
-
